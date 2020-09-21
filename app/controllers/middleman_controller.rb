31e500ec5bf2f5dbc47a99909b801c5b1747a7dc
NyaradzoUBere <nybe5598@colorado.edu>
2020-09-21 15:57:06 -0600

    Render json in Middleman controller

--- app/controllers/middleman_controller.rb
+++ app/controllers/middleman_controller.rb
@@ -2,6 +2,6 @@ class MiddlemanController < ApplicationController
     def api_call
         response = RestClient.get('https://newsapi.org/v2/everything?q=%22george_floyd%22&language=en&from=2020-08-21&sortBy=publishedAt&apiKey=540c18062492423c90061fefc752f212')
         parsed_response = JSON.parse(response)
-        byebug
+        render json: parsed_response
     end
 end
