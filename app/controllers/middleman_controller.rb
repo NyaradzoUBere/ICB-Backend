d621465adfff32e5b496b7889db5f3ccc5507bf4
NyaradzoUBere <nybe5598@colorado.edu>
2020-09-22 11:31:14 -0600

    Automatically update time field of API

--- app/controllers/middleman_controller.rb
+++ app/controllers/middleman_controller.rb
@@ -1,7 +1,9 @@
 class MiddlemanController < ApplicationController
+
     def api_call
-        response = RestClient.get('https://newsapi.org/v2/everything?q=%22george_floyd%22&language=en&from=2020-08-21&sortBy=publishedAt&apiKey=540c18062492423c90061fefc752f212')
+        response = RestClient.get("https://newsapi.org/v2/everything?q=%22george_floyd%22&language=en&from=#{Time.now.year}-#{Time.now.month - 1}-#{Time.now.day}&sortBy=publishedAt&apiKey=540c18062492423c90061fefc752f212")
         parsed_response = JSON.parse(response)
         render json: parsed_response
     end
+    
 end
