a1f79b119d537549ee05d1f1f692d0582e14e5f0
NyaradzoUBere <nybe5598@colorado.edu>
2020-09-21 18:26:58 -0600

    Add show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :people, only: [:index]
+  resources :people, only: [:index, :show]
   get '/middleman', to: 'middleman#api_call'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
